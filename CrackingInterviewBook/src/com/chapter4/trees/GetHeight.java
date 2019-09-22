8766a5799ad28122a4412073b8551b86d31bd55c
cryptonkid <cryptonkid@gmail.com>
2019-09-22 16:12:16 +0530

    Height of TreeIterative&Recurvsive

--- CrackingInterviewBook/src/com/chapter4/trees/GetHeight.java
+++ CrackingInterviewBook/src/com/chapter4/trees/GetHeight.java
@@ -1,5 +1,9 @@
 package com.chapter4.trees;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Queue;
+
 /**
  * 
  * @author cryptonkid
@@ -12,20 +16,33 @@ public class GetHeight {
 		int height = ((findHeightRecursive(root.left)+1)>(findHeightRecursive(root.right)+1)?(findHeightRecursive(root.left)+1):(findHeightRecursive(root.right)+1));
 		return height;
 	}
-	
+	//Iterative solution: One need to do the level order traversal.  
 	public static int findHeightIterative(Node root) {
-		int height = 0;
-		if(root == null) {
+		int height =0;
+		if(root == null)
 			return height;
-		}
+		Deque<Node> queue = new ArrayDeque<Node>();
+		queue.add(root);
 		while(true) {
-			if(root.left!= null) {
-				height = 0;//todo fill with correct values
-				
+			
+			int noOfNodeOnCurrentLevel = queue.size();
+			if(noOfNodeOnCurrentLevel == 0)
+				return height;
+			
+			// Dequeue all nodes of current level and Enqueue all 
+            // nodes of next level 
+			while(noOfNodeOnCurrentLevel > 0) {
+				Node tempNode = queue.peek();
+				if(tempNode != null) {
+					queue.remove();
+					if(tempNode.left != null)
+						queue.add(tempNode.left);
+					if(tempNode.right != null)
+						queue.add(tempNode.right);
+				}
+				noOfNodeOnCurrentLevel--;
 			}
-			else {
-				
-			}
+			height++;//Increase the height.
 		}
 	}
 }
