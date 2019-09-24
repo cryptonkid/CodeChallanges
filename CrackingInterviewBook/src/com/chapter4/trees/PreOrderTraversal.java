ac6b2dd5666e9bf478a73cd9175a2b0cdf4afbba
cryptonkid <cryptonkid@gmail.com>
2019-09-24 06:01:21 +0530

    PreOrder Traversal

--- CrackingInterviewBook/src/com/chapter4/trees/PreOrderTraversal.java
+++ CrackingInterviewBook/src/com/chapter4/trees/PreOrderTraversal.java
@@ -1,5 +1,34 @@
 package com.chapter4.trees;
 
-public class PreOrderTraversal {
+import java.util.ArrayDeque;
+import java.util.Deque;
 
+public class PreOrderTraversal {
+	public static void main() {
+		
+	}
+	
+	public void preOrderRecursive(Node root) {
+		if(root == null )
+			return;
+		System.out.println(root.data + " ");
+		preOrderRecursive(root.left);
+		preOrderRecursive(root.right);
+	}
+	//We will use queue for preorder
+	public void preOrderIterative(Node root) {
+		if(root == null)
+			return;
+		Deque<Node> queue = new ArrayDeque<Node>();
+		queue.add(root);
+		while(!queue.isEmpty()) {
+			Node temp = queue.peek();
+			System.out.println(temp.data);
+			if(temp.left != null)
+				queue.add(temp.left);
+			if(temp.right != null)
+				queue.add(temp.right);
+			queue.remove();
+		}
+	}
 }
