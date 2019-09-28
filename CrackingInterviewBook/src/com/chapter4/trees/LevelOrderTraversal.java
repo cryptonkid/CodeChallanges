f97ada21dabbfe1b8ddc80a18f95c11b2f50636e
cryptonkid <cryptonkid@gmail.com>
2019-09-28 07:24:08 +0530

    Level Order Traversal

---
+++ CrackingInterviewBook/src/com/chapter4/trees/LevelOrderTraversal.java
@@ -0,0 +1,29 @@
+package com.chapter4.trees;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class LevelOrderTraversal {
+
+	/**
+	 * For level order traversal we will use qeueue
+	 * time and space complexity : 0(n)
+	 * @param root
+	 */
+	public static void levelOrderTraversal(Node root) {
+		Deque<Node> queue = new ArrayDeque<Node>();
+		if (root != null)
+			queue.add(root);
+		while (!queue.isEmpty()) {
+			Node current = queue.poll();
+			if (current != null) {
+				System.out.println(current.data);
+				if (current.left != null)
+					queue.add(root.left);
+				if (current.right != null)
+					queue.add(root.right);
+			}
+
+		}
+	}
+}
