a7ed79d25e2c9993e64c1b09a08f771fb75a1bc9
vivek.singh <vivek.singh@247-inc.com>
2020-05-23 04:14:37 +0530

    DP knapsack01 fixes

--- LearnDP/src/com/learndp/knapsack01/group/Knapsack01.java
+++ LearnDP/src/com/learndp/knapsack01/group/Knapsack01.java
@@ -1,5 +1,46 @@
 package com.learndp.knapsack01.group;
+/**
+ * 0 - 1 Knapsack Problem
+You are given weights and values of N items, put these items in a knapsack of capacity W to get the maximum total value in the knapsack. Note that we have only one quantity of each item.
+In other words, given two integer arrays val[0..N-1] and wt[0..N-1] which represent values and weights associated with N items respectively. Also given an integer W which represents knapsack capacity, find out the maximum value subset of val[] such that sum of the weights of this subset is smaller than or equal to W. You cannot break an item, either pick the complete item, or don’t pick it (0-1 property).
 
+Input:
+The first line of input contains an integer T denoting the number of test cases. Then T test cases follow. Each test case consists of four lines.
+The first line consists of N the number of items.
+The second line consists of W, the maximum capacity of the knapsack.
+In the next line are N space separated positive integers denoting the values of the N items,
+and in the fourth line are N space separated positive integers denoting the weights of the corresponding items.
+
+Output:
+For each testcase, in a new line, print the maximum possible value you can get with the given conditions that you can obtain for each test case in a new line.
+
+Expected Time Complexity: O(N*W).
+Expected Auxiliary Space: O(N*W)
+
+Constraints:
+1 ≤ T ≤ 100
+1 ≤ N ≤ 1000
+1 ≤ W ≤ 1000
+1 ≤ wt[i] ≤ 1000
+1 ≤ v[i] ≤ 1000
+
+Example:
+Input:
+2
+3
+4
+1 2 3
+4 5 1
+3
+3
+1 2 3
+4 5 6
+Output:
+3
+0
+ * @author Vivek.Singh
+ *
+ */
 public class Knapsack01 {
 
 	public static void main(String[] args) {
@@ -7,8 +48,8 @@ public class Knapsack01 {
 		
 		int val[] = {22, 20, 15, 30, 24, 54, 21, 32, 18, 25};
         int wt[] = {4, 2, 3, 5, 5, 6, 9, 7, 8, 10};
-        //int r = k.recursiveKnapsack(wt,val, 30,wt.length-1); //causes stack over flow. 
-        //System.out.println(r);
+        int r = k.recursiveKnapsack(wt,val, 30,wt.length-1); //causes stack over flow. 
+        System.out.println(r);
         
         int val1[] = {4,5,1};
         int wt1[] = {1,2,3};
@@ -32,9 +73,9 @@ public class Knapsack01 {
        /**
         * TopDown Approach
         */
-        k.topDownKnapsack01(wt, val, 30, wt.length);
-        k.topDownKnapsack01(wt1, val1, 4, wt1.length);
-        k.topDownKnapsack01(wt2, val2, 3, wt2.length);
+        System.out.println(k.topDownKnapsack01(wt, val, 30, wt.length));
+        System.out.println(k.topDownKnapsack01(wt1, val1, 4, wt1.length));
+        System.out.println(k.topDownKnapsack01(wt2, val2, 3, wt2.length));
         
 
 	}
@@ -85,16 +126,14 @@ public class Knapsack01 {
 					dp[i][j] = 0;
 					continue;
 				}
-				if(weights[i] > knapsackCapacity) {
+				if(weights[i-1] > j) {  // j not KnapsackCapacity as j represents the current subproblem/capacity. 
 					dp[i][j] = dp[i-1][j];
 				}
 				else
-					dp[i][j] = Math.max(values[i]+dp[i-1][j-weights[i-1]],
-										dp[i-1][j-weights[i-1]]); //note : how weights value is being subtracted from j as j is now knapsackCapacity.
+					dp[i][j] = Math.max(values[i-1]+dp[i-1][j-weights[i-1]],
+										dp[i-1][j]); //note : how weights value is being subtracted from j as j is now knapsackCapacity.
 			}
 		}
 		return dp[n][knapsackCapacity];
 	}
-	
-	
 }
