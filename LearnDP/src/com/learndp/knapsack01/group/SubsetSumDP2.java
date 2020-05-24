f386f7fbdb1d18465b554ae0ae005790c8f41e75
vivek.singh <vivek.singh@247-inc.com>
2020-05-24 20:30:46 +0530

    Counting the number of subsets for a given sum

--- LearnDP/src/com/learndp/knapsack01/group/SubsetSumDP2.java
+++ LearnDP/src/com/learndp/knapsack01/group/SubsetSumDP2.java
@@ -1,7 +1,9 @@
 package com.learndp.knapsack01.group;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 /**
  * https://www.geeksforgeeks.org/subset-sum-problem-dp-25/
@@ -22,13 +24,59 @@ import java.io.InputStreamReader;
  * @author Vivek.Singh
  *
  */
+
+/**
+ * Subset Sum Problem
+Given a set of numbers, check whether it can be partitioned into two subsets such that the sum of elements in both subsets is same or not.
+
+Input:
+The first line contains an integer 'T' denoting the total number of test cases. Each test case consists of two lines. First-line contains 'N', representing the number of elements in the set and the second line contains the elements of the set.
+
+Output:
+For each test case, print YES if the partition is possible, otherwise print NO.
+
+Expected Time Complexity: O(N*N)
+Expected Auxiliary Space: O(N)
+
+User Task:
+You don't need to read input or print output, taking inputs and printing is done by the driver code. Your task is to complete the findPartition() function and return true if the given set can be partitioned into two subsets such that the sum of elements in both subsets is equal, else return false.
+
+Constraints:
+1 <= T <= 100
+1 <= N <= 100
+0 <= arr[i] <= 1000
+
+Example:
+Input:
+2
+4
+1 5 11 5
+3
+1 3 5 
+Output:
+YES
+NO
+
+Explanation:
+Testcase 1: There exists two subsets such that {1, 5, 5} and {11}.
+ * @author Vivek.Singh
+ *
+ */
 public class SubsetSumDP2 {
 
-	public static void main(String[] args) {
-		//BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		//int testCases = Integer.parseInt(br.readLine());
-		System.out.println(findPartition(new int[] {1,2,3},3));
-		//System.out.println(findPartition(new int[] {478,757,314,471,729,100,459,618}, 8));
+	public static void main(String[] args) throws IOException{
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		int testCases = Integer.parseInt(br.readLine());
+//		while(testCases-- > 0) {
+//			
+//		}
+		
+		//System.out.println(findEqualSumPartitionSetExists(new int[] {1,2,3},3));
+		//System.out.println(findEqualSumPartitionSetExists(new int[] {478,757,314,471,729,100,459,618}, 8));
+		System.out.println("============ If Subset exist with sum of 3 ==============");
+		System.out.println(topDownIsSubsetSumExists(new int[] {1,2,3},3,3));
+		System.out.println("============ Count of Subsets of sum 3 ==============");
+		System.out.println(countNoOfSubSets(new int[] {1,2,3},3,3));
 		
 		
 	}
@@ -49,17 +97,17 @@ public class SubsetSumDP2 {
 	 * i.e. when sum = 0 return true
 	 * @return
 	 */
-	public boolean recursiveSubsetSum(int input[], int sum , int n) {
+	public boolean recursiveIsSubsetSumExists(int input[], int sum , int n) {
 		if(n == 0 )
 			return false;
 		if(sum == 0)
 			return true;
 		else if( input[n] > sum ) {
-			return recursiveSubsetSum(input, sum, n-1); //do not include the number.
+			return recursiveIsSubsetSumExists(input, sum, n-1); //do not include the number.
 		}
 		else {
 			//As math.max is for value but now we are trying of find if the sum exist or not. 
-			return (recursiveSubsetSum(input, sum-input[n], n-1) || recursiveSubsetSum(input, sum, n-1) );
+			return (recursiveIsSubsetSumExists(input, sum-input[n], n-1) || recursiveIsSubsetSumExists(input, sum, n-1) );
 		}
 		
 	}
@@ -67,31 +115,70 @@ public class SubsetSumDP2 {
 	/**
 	 * Top Down approach : 
 	 * 1) create an array of size n+1, sum+1
+	 * Time complexity:  (n*W) i.e. W is weight/Sum and n is number of elements in a set. 
 	 */
-	public boolean topDownSubsetSum(int arr[],int sum,int n) {
+	public static boolean topDownIsSubsetSumExists(int arr[],int sum,int n) {
 		boolean dp[][] = new boolean[n+1][sum+1];
 		for(int i = 0;i<=n;i++) {
 			for(int j=0;j<=sum;j++) {
-				if(i == 0)
-					dp[i][j] = false;
-				if(j == 0)
-					dp[i][j] = true;
-				else if(arr[i] > j)//j not sum 
-					dp[i][j] = false;
-				else
-					dp[i][j] = dp[i-1][j-arr[i-1]] || dp[i-1][j];
+				if(i==0 || j == 0) {
+					if(i == 0)
+						dp[i][j] = false;
+					if(j == 0)
+						dp[i][j] = true;
+				}
+				else {
+					if(arr[i-1] > j)//j not sum
+						dp[i][j] = false;
+					else
+						dp[i][j] = dp[i-1][j-arr[i-1]] || dp[i-1][j];
+				}
 			}
 		}
-		return dp[n+1][sum+1];
+		for (boolean[] row : dp) 
+            System.out.println(Arrays.toString(row));
+		return dp[n][sum];
 	}
+	
+	/*
+	 * Function to find number of Subset with a given sum in a set
+	 * Logic : It is same as the if subset sum exists. As every true
+	 * 	will lead to addition of count.
+	 */
+	public static int countNoOfSubSets(int []a,int sum, int n) {
+		int dp[][] = new int[n+1][sum+1];
+		for(int i=0;i<n+1;i++) {
+			for(int j=0;j<sum+1;j++) {
+				if(i == 0 || j ==0) {
+					if(i == 0)
+						dp[i][j]=0;
+					if(j==0)
+						dp[i][j] = 1 ; //null set (null set is part of every set)
+				}
+				else {
+					if(a[i-1] > j)
+                        dp[i][j] = dp[i-1][j];
+                    else
+                        dp[i][j] = dp[i-1][j-a[i-1]] + dp[i-1][j];
+				}
+			}
+		}
+		for (int[] row : dp) 
+            System.out.println(Arrays.toString(row)); 
+		return dp[n][sum];
+	}
+	 
 	/**
 	 * https://www.geeksforgeeks.org/subset-sum-problem-dp-25/ 
-	 * Here final sum is not given .
+	 * Here final sum is not given . //Equal sum Partition
+	 * 1) sum of all the elements must be even : one can then divide the partition into 2 equal partitions. 
+	 * 2) then find if the sum of half of sum of all the elements exists in the set or not
+	 * 		Just like isSubsetSumExists.
 	 * @param a
 	 * @param n
 	 * @return
 	 */
-	public static boolean findPartition(int[] a, int n) {
+	public static boolean findEqualSumPartitionSetExists(int[] a, int n) {
         int sum = 0;
         for(int i =0 ;i<n;i++)
          sum = sum + a[i];
@@ -118,7 +205,8 @@ public class SubsetSumDP2 {
                 }
             }
             return dp[n][sum];
-        }
-	}
-	
+        }//End of else
+        
+	}//end of findPartition 
+
 }
