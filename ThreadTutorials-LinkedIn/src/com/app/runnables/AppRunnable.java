6779a2b28def8b45ae06a7b614ce8b9b6e3edfb9
vivek.singh <vivek.singh@247-inc.com>
2020-06-24 19:14:58 +0530

    Implementing Thread using Runnable interface-full

---
+++ ThreadTutorials-LinkedIn/src/com/app/runnables/AppRunnable.java
@@ -0,0 +1,15 @@
+package com.app.runnables;
+
+public class AppRunnable implements Runnable{
+
+    @Override
+    public void run() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    public static void main(String[] args) {
+        AppRunnable runnable = new AppRunnable();
+    }
+    
+    
+}
--- ThreadTutorials-LinkedIn/src/com/app/tests/TestRunnable.java
+++ ThreadTutorials-LinkedIn/src/com/app/tests/TestRunnable.java
@@ -16,7 +16,7 @@ public class TestRunnable {
 
     public static void main(String[] args) {
         Runnable runnable = () -> {
-            try (BufferedReader reader = new BufferedReader(new FileReader(new File("C:\\Users\\Ketkee Aryamane\\Desktop\\Exercise Files\\Chapter2\\02_03\\begin\\sample.txt")))) {
+            try (BufferedReader reader = new BufferedReader(new FileReader(new File("C:\\Users\\cryptonkid\\Desktop\\Exercise Files\\Chapter2\\02_03\\begin\\sample.txt")))) {
                 String line = null;
                 while ((line = reader.readLine()) != null) {
                     System.out.println(Thread.currentThread().getName() + " reading the line: " + line);
