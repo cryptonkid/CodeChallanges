6cab30ec0721a6d8a1220987e160c2a5942c02c4
vivek.singh <vivek.singh@247-inc.com>
2020-06-24 19:15:58 +0530

    Implementing Thread using Thread Class complete

--- ThreadTutorials-LinkedIn/src/com/app/tests/TestThreads.java
+++ ThreadTutorials-LinkedIn/src/com/app/tests/TestThreads.java
@@ -4,11 +4,11 @@ import com.app.runnables.AppThread;
 public class TestThreads {
     
     public static void main(String[] args) {
-        AppThread thread1 = new AppThread();//NEW
+        AppThread thread1 = new AppThread();// NEW
         AppThread thread2 = new AppThread();
         AppThread thread3 = new AppThread();
         
-        thread1.start();//RUNNABLE
+        thread1.start();// RUNNABLE
         thread2.start();
         thread3.start();
         //execute run // RUNNING
