c25d2f3d134a068fb4fda111d1fd4a4aa29626b5
vivek.singh <vivek.singh@247-inc.com>
2020-08-05 00:39:04 +0530

    Using 2 pointer approach

--- LeetCode/src/StringPalindrome.java
+++ LeetCode/src/StringPalindrome.java
@@ -43,6 +43,21 @@ public class StringPalindrome {
 
 		return builder.toString().equals(builder.reverse().toString());
 	}
+	public boolean isPalindrome2PointerApproach(String s) {
+	    for (int i = 0, j = s.length() - 1; i < j; i++, j--) {
+	      while (i < j && !Character.isLetterOrDigit(s.charAt(i))) {
+	        i++;
+	      }
+	      while (i < j && !Character.isLetterOrDigit(s.charAt(j))) {
+	        j--;
+	      }
+
+	      if (i < j && Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j)))
+	        return false;
+	    }
+
+	    return true;
+	  }
 
 	/**
 	 * uppercase ascii 65-90
